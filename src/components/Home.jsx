26cf67c10b7da5b3bd45570d35a6a0774ed8493c
Jonathan Martinez <68358371+jmartin-code@users.noreply.github.com>
2020-12-09 12:34:42 -0500

    refactoring the code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,6 +22,7 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: "10px",
     padding: "50px 50px 20px 50px",
     boxShadow: "0 2px 5px"
+    
   },
   profileImage: {
     textAlign: "center",
