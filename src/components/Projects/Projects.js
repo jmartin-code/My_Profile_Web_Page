c230a7e67d84dd9ebbce047ffd86013c59f263e3
Jonathan Martinez <68358371+jmartin-code@users.noreply.github.com>
2021-02-28 00:09:14 -0500

    refactoring projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,85 +1,37 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import ProfileImg from "../../../src/Images/ProfileImg.jpg";
-import useStyles from "./styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import useStyles from "./styles.js";
 
-const Projects = () => {
-  // const [spacing, setSpacing] = React.useState(10);
+export default function ImgMediaCard() {
   const classes = useStyles();
 
-  // const handleChange = (event) => {
-  //   setSpacing(Number(event.target.value));
-  // };
-
   return (
-    <Grid container className={classes.root}>
-      <Typography variant="h5" align="center">
-        WORK IN PROGRESS
-      </Typography>
-      <Grid item xs={12}>
-        <Grid container justify="center" spacing={10}>
-          {/* {[0, 1, 2].map((value) => ( */}
-          <Grid item>
-            <Paper className={classes.paper}>
-              <Grid item align="center">
-                <img height="200" width="200" draggable="false" className={classes.img} alt="" src={ProfileImg} />
-              </Grid>
-              <Grid item align="center">
-                <Typography gutterBottom variant="subtitle1">
-                  In Progress
-                </Typography>
-                <Typography variant="h5" gutterBottom>
-                  Full resolution 1920x1080 • JPEG
-                </Typography>
-                <Typography variant="h5" color="textSecondary">
-                  ID: 1030114
-                </Typography>
-              </Grid>
-            </Paper>
-          </Grid>
-          <Grid item>
-            <Paper className={classes.paper}>
-              <Grid item align="center">
-                <img height="100" width="100" draggable="false" className={classes.img} alt="" src={ProfileImg} />
-              </Grid>
-              <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
-                  Standard license
-                </Typography>
-                <Typography variant="h5" gutterBottom>
-                  Full resolution 1920x1080 • JPEG
-                </Typography>
-                <Typography variant="h5" color="textSecondary">
-                  ID: 1030114
-                </Typography>
-              </Grid>
-            </Paper>
-          </Grid>
-          <Grid item>
-            <Paper className={classes.paper}>
-              <Grid item align="center">
-                <img height="100" width="100" draggable="false" className={classes.img} alt="" src={ProfileImg} />
-              </Grid>
-              <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
-                  Standard license
-                </Typography>
-                <Typography variant="h5" gutterBottom>
-                  Full resolution 1920x1080 • JPEG
-                </Typography>
-                <Typography variant="h5" color="textSecondary">
-                  ID: 1030114
-                </Typography>
-              </Grid>
-            </Paper>
-          </Grid>
-          {/* ))} */}
-        </Grid>
-      </Grid>
-    </Grid>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia component="img" alt="Contemplative Reptile" height="140" image="/static/images/cards/contemplative-reptile.jpg" title="Contemplative Reptile" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
   );
-};
-
-export default Projects;
+}
--- src/components/Projects/styles.js
+++ src/components/Projects/styles.js
@@ -2,19 +2,6 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   root: {
-    margin: "100px auto",
-    flexGrow: 1,
-  },
-  paper: {
-    height: 400,
-    width: 300,
-    background: "#D1D0CE",
-  },
-  img: {
-    padding: "10px",
-    margin: "auto",
-    display: "block",
-    maxWidth: "100%",
-    maxHeight: "100%",
+    maxWidth: 345,
   },
 }));
